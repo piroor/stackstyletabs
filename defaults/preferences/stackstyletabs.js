a36b36fd1340a7e7a049cacc278b02fb0c8cbf26
piro <piro@599a83e7-65a4-db11-8015-0010dcdd6dc2>
2007-05-13 20:51:54 +0000

    拡張機能名と説明文をローカライズした
    git-svn-id: https://www.cozmixng.org/repos/piro/stackstyletabs/trunk@575 599a83e7-65a4-db11-8015-0010dcdd6dc2

---
+++ defaults/preferences/stackstyletabs.js
@@ -0,0 +1,2 @@
+pref("extensions.{149C6CC6-EC62-4ebd-B719-3C2E867930C7}.name", "chrome://stackstyletabs/locale/stackstyletabs.properties");
+pref("extensions.{149C6CC6-EC62-4ebd-B719-3C2E867930C7}.description", "chrome://stackstyletabs/locale/stackstyletabs.properties");
